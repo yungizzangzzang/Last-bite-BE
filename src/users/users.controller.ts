a152affe130861d61933f494164ff443b0ed6bc6
진찬호 <chanhojin94@kakao.com>
2023-10-07 16:54:54 +0900

    Feat: 로그인 및 회원가입 로그인 및 회원가입 api crud 작성 로그인 및 회원가입 api 작성

---
+++ src/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+
+@Controller('users')
+@ApiTags('users')
+export class UsersController {
+    constructor(private readonly usersService:UsersService){}
+}
