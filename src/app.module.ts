ce9fca8ff6c8d7c37e7a8fcb3243640796a0b920
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-07 15:42:49 +0900

    Setting: 인터셉터, 로그 미들웨어, 필터, config, swagger라이브러리 추가
    1. logger.middleware.ts
    - 서버로깅을 위해 로그 미들웨어 추가
    
    2. undefinedToNull.interceptor.ts
    - 클라이언트에 undefined로 보내질 데이터들을 Null 타입으로 변환해서 보내기 위해 interceptor 추가
    
    3. success.interceptor.ts
    - 성공요청을 일관되게 보내주기위해 interceptor 추가
    
    4. http-exception.filter.ts
    - 일관된 오류메시지를 전달하기 위해 http-exception filter 추가
    
    5. user.decorator.ts
    - request.user를 사용하기 위해 user.decorator.ts 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.develpoment' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
