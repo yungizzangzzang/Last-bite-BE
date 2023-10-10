382c73d8a5c5ef5be83aa067dfd5f49039b3c93f
yungi2383 <g830201@nate.com>
2023-10-11 03:16:07 +0900

    Feat: items 스웨거 추가

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,4 +1,4 @@
-import { PickType } from '@nestjs/mapped-types';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { ItemEntity } from '../entities/item.entity';
 
@@ -12,9 +12,19 @@ export class CreateItemDto extends PickType(ItemEntity, [
 ] as const) {
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    type: Number,
+    description: '할인 시작 시간',
+    example: '19',
+  })
   startTime!: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    type: Number,
+    description: '할인 마감 시간',
+    example: '21',
+  })
   endTime!: number;
 }
