55dc2255360579effcae014f35e161d6366fb5fa
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-14 16:19:11 +0900

    Feat: Bull queue설치, 적용 완료, redis와 연결 완료

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from 'src/items/items.module';
+import { JobsModule } from 'src/jobs/jobs.module';
 import { OrderItemsModule } from 'src/order-items/order-items.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { OrdersController } from './orders.controller';
@@ -7,7 +8,7 @@ import { OrdersRepository } from './orders.repository';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [PrismaModule, OrderItemsModule, ItemsModule],
+  imports: [PrismaModule, OrderItemsModule, ItemsModule, JobsModule],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
 })
