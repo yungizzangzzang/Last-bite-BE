715f841bde7a82178e1d9b9291b0a3e1edd4e1d7
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-10-26 16:46:29 +0900

    Fix: 모듈에 대한 테스트코드 때문에 테스트가 너무 오래걸리는 것 같아 주석처리

--- src/likes/likes.module.spec.ts
+++ src/likes/likes.module.spec.ts
@@ -1,37 +1,37 @@
-import { forwardRef } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { StoresModule } from 'src/stores/stores.module';
-import { LikesController } from './likes.controller';
-import { LikesModule } from './likes.module';
-import { LikesRepository } from './likes.repository';
-import { LikesService } from './likes.service';
+// import { forwardRef } from '@nestjs/common';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { PrismaModule } from 'src/prisma/prisma.module';
+// import { StoresModule } from 'src/stores/stores.module';
+// import { LikesController } from './likes.controller';
+// import { LikesModule } from './likes.module';
+// import { LikesRepository } from './likes.repository';
+// import { LikesService } from './likes.service';
 
-describe('LikesModule', () => {
-  let likesModule: LikesModule;
-  let module: TestingModule;
+// describe('LikesModule', () => {
+//   let likesModule: LikesModule;
+//   let module: TestingModule;
 
-  beforeEach(async () => {
-    module = await Test.createTestingModule({
-      imports: [PrismaModule, forwardRef(() => StoresModule), LikesModule],
-    }).compile();
+//   beforeEach(async () => {
+//     module = await Test.createTestingModule({
+//       imports: [PrismaModule, forwardRef(() => StoresModule), LikesModule],
+//     }).compile();
 
-    likesModule = module.get<LikesModule>(LikesModule);
-  });
+//     likesModule = module.get<LikesModule>(LikesModule);
+//   });
 
-  it('should be defined', () => {
-    expect(likesModule).toBeDefined();
-  });
+//   it('should be defined', () => {
+//     expect(likesModule).toBeDefined();
+//   });
 
-  it('Controller에 LikesController가 있어야 함.', () => {
-    expect(module.get<LikesController>(LikesController)).toBeDefined();
-  });
+//   it('Controller에 LikesController가 있어야 함.', () => {
+//     expect(module.get<LikesController>(LikesController)).toBeDefined();
+//   });
 
-  it('프로바이더로 LikesService가 있어야 함.', () => {
-    expect(module.get<LikesService>(LikesService)).toBeDefined();
-  });
+//   it('프로바이더로 LikesService가 있어야 함.', () => {
+//     expect(module.get<LikesService>(LikesService)).toBeDefined();
+//   });
 
-  it('프로바이더로 LikesRepository 있어야 함.', () => {
-    expect(module.get<LikesRepository>(LikesRepository)).toBeDefined();
-  });
-});
+//   it('프로바이더로 LikesRepository 있어야 함.', () => {
+//     expect(module.get<LikesRepository>(LikesRepository)).toBeDefined();
+//   });
+// });
