e38e0f36f0cc7f46911592c97d051a7df64a1075
yungi2383 <g830201@nate.com>
2023-11-05 01:38:35 +0900

    Fix: userRedis 생성, point 로직 추가

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,8 @@
 import { InjectQueue } from '@nestjs/bull';
-import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Queue } from 'bull';
-import { ItemsRepository } from 'src/items/items.repository';
-import { OrderItemsRepository } from 'src/order-items/order-items.repository';
+import { Redis } from 'ioredis';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderOrderItemDto } from './dto/create-order.dto';
 import { OneOrderDTO } from './dto/get-one-order.dto';
 import { UserOrdersDTO } from './dto/get-user-orders.dto';
@@ -11,24 +10,79 @@ import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
+  private readonly userRedis: Redis;
   constructor(
     @InjectQueue('orders') private ordersQueue: Queue, // bullqueue DI
     private readonly ordersRepository: OrdersRepository,
-    private readonly orderItemsRepository: OrderItemsRepository,
-    private readonly eventEmitter: EventEmitter2,
-    private readonly itemsRepository: ItemsRepository,
-  ) {}
+    private readonly prisma: PrismaService,
+  ) {
+    this.userRedis = new Redis({
+      port: 7001,
+      host: 'localhost',
+    });
+  }
+
+  async updateRedisUserPoint(
+    userId: number,
+    totalPrice: number,
+  ): Promise<number> {
+    // userId로 redis에서 포인트 조회
+    let getRedisPoint = await this.userRedis.get(userId.toString());
+
+    let redisPoint = getRedisPoint ? +getRedisPoint : -1;
+    // redis에 user 정보 없을 때 DB 조회
+    if (redisPoint === -1) {
+      const user = await this.prisma.users.findUnique({
+        where: { userId },
+        select: { point: true },
+      });
+
+      // user 및 user.point 정보 없을 때
+      if (!user || !user.point) {
+        throw new HttpException(
+          { message: '사용자 정보를 확인할 수 없습니다.' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      // DB에서 조회한 user 정보(point) redis에 넣기
+      const userPoint = user.point;
+
+      await this.userRedis.set(userId.toString(), userPoint.toString());
+      redisPoint = userPoint;
+    }
+
+    // redisPoint < totalPrice
+    if (redisPoint < totalPrice) {
+      throw new HttpException(
+        { message: '포인트를 충전해주세요.' },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // redis point 차감 (point - totalPrice)
+    await this.userRedis.decrby(userId.toString(), totalPrice);
+
+    return redisPoint - totalPrice;
+  }
 
   async createOrder(
     createOrderOrderItemDto: CreateOrderOrderItemDto,
     userId: number,
   ) {
     try {
+      const redisUserPoint = await this.updateRedisUserPoint(
+        userId,
+        createOrderOrderItemDto.totalPrice,
+      );
+
       const result = await this.ordersQueue.add('create', {
         createOrderOrderItemDto,
         userId,
+        redisUserPoint,
       });
-      return result;
+
+      return { result };
     } catch (error) {
       console.error(error);
     }
