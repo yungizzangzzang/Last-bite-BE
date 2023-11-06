5896222106315ef797eba5528398996ef8d045e9
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-11-07 02:40:05 +0900

    Feat: 주문 생성 컨슈머 서버 분리

--- consumer-createOrder/src/app.module.ts
+++ consumer-createOrder/src/app.module.ts
@@ -3,13 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { ItemsModule } from './items/items.module';
-import { OrderItemsModule } from './order-items/order-items.module';
 import { OrdersModule } from './orders/orders.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
-import { StoresModule } from './stores/stores.module';
-import { AuthModule } from './users/auth/auth.module';
 
 @Module({
   imports: [
@@ -17,11 +13,7 @@ import { AuthModule } from './users/auth/auth.module';
       isGlobal: true,
     }),
     PrismaModule,
-    StoresModule,
-    ItemsModule,
-    AuthModule,
     OrdersModule,
-    OrderItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
--- consumer-createOrder/src/main.ts
+++ consumer-createOrder/src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
@@ -10,26 +9,6 @@ import { UndefinedToNullInterceptor } from './common/interceptors/undefinedToNul
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 8001;
-
-  const config = new DocumentBuilder()
-    .setTitle('Yungizzang')
-    .setDescription('Yungizzang API doc')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'jwt',
-    )
-    .setVersion('1.0')
-    .addTag('Yungizzang')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   app.useGlobalInterceptors(
     new UndefinedToNullInterceptor(),
