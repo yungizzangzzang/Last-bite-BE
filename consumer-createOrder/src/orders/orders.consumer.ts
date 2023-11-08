52fd462a3a181bfc7ff683faf9bc036dfc78388f
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-11-08 13:30:53 +0900

    Feat: Bull queue -> 레디스 스트림으로 변환 작업 완료, 컨슈머 그룹 적용

--- consumer-createOrder/src/orders/orders.consumer.ts
+++ consumer-createOrder/src/orders/orders.consumer.ts
@@ -14,69 +14,71 @@ export class CreateOrderStreamConsumer {
     return client;
   }
 
-  onModuleInit() {
+  constructor(private readonly prisma: PrismaService) {
+    this.createOrderStream = this.createRedisClient(7004);
     this.consumeCreateOrderStream();
   }
 
-  constructor(private readonly prisma: PrismaService) {
-    this.createOrderStream = this.createRedisClient(7004);
-  }
-
   async consumeCreateOrderStream() {
+    const consumerName = process.env.HOST as string;
+    const groupName = 'createOrderGroup';
     const streamName = 'createOrderStream';
-    let lastId = '$';
 
     while (true) {
       try {
-        const messages = await this.createOrderStream.xread(
+        const messages: any = await this.createOrderStream.xreadgroup(
+          'GROUP',
+          groupName,
+          consumerName,
+          'COUNT',
+          '1',
           'BLOCK',
-          0,
+          '1000',
           'STREAMS',
           streamName,
-          lastId,
+          '>',
         );
 
-        if (!messages) {
-          await new Promise((resolve) => setTimeout(resolve, 1000));
+        if (!messages || messages.length === 0 || messages[0][1].length === 0) {
           continue;
         }
 
         for (const [, streamMessages] of messages) {
           for (const [messageId, messageFields] of streamMessages) {
-            const userIdIndex = messageFields.indexOf('userId');
-            const detailsIndex = messageFields.indexOf('details');
-
-            if (userIdIndex !== -1 && detailsIndex !== -1) {
-              const userId = parseInt(messageFields[userIdIndex + 1], 10);
-              const details = JSON.parse(messageFields[detailsIndex + 1]);
-
-              await this.createOrderAndOrderItems(userId, details);
-
-              await this.createOrderStream.xack(
-                streamName,
-                'createOrderGroup',
-                messageId,
-              );
-            }
-
-            lastId = messageId;
+            await this.processMessage(messageId, messageFields);
+            await this.createOrderStream.xack(streamName, groupName, messageId);
           }
         }
       } catch (error) {
-        console.error('스트림 처리 중 오류가 발생했습니다:', error);
+        console.error('스트림 처리 중 오류:', error);
       }
     }
   }
 
-  private async createOrderAndOrderItems(
-    userId: number,
-    details: {
-      discount: number;
-      storeId: number;
-      totalPrice: number;
-      items: { itemId: number; count: number }[];
-    },
-  ) {
+  private async processMessage(
+    messageId: string,
+    messageFields: any[],
+  ): Promise<boolean> {
+    try {
+      const userIdIndex = messageFields.indexOf('userId');
+      const detailsIndex = messageFields.indexOf('details');
+
+      if (userIdIndex !== -1 && detailsIndex !== -1) {
+        const userId = parseInt(messageFields[userIdIndex + 1], 10);
+        const details = JSON.parse(messageFields[detailsIndex + 1]);
+
+        await this.createOrderAndOrderItems(userId, details);
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      console.error(`메시지 처리 실패 - ${messageId}:`, error);
+      return false;
+    }
+  }
+
+  private async createOrderAndOrderItems(userId: number, details: any) {
     const { discount, storeId, totalPrice, items } = details;
 
     const order = await this.prisma.orders.create({
